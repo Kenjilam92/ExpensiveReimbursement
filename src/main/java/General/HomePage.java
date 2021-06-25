e9860e6e07d65040c74191159047da86cee16824
kenjilam92 <kenji.lam.92@gmail.com>
2021-06-24 22:10:33 -0400

    found html location

--- src/main/java/General/HomePage.java
+++ src/main/java/General/HomePage.java
@@ -1,8 +1,33 @@
 package General;
 import tools.*;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HomePage extends HttpServlet implements Print,GetTextFile {
-	
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomePage extends HttpServlet implements Print {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public HomePage() {
+		super();
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		printBorder();
+		println("HomePage run");
+		RequestDispatcher view = request.getRequestDispatcher("/html/hompage1.html");
+//		
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println(getTextFile("HomePage.html"));
+		view.forward(request, response)	;
+	}
 }
